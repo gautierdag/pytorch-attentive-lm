1e5e7475f62812977652c67e8f431da920d51c12
Gautier Dagan <gautierdagan2017@u.northwestern.edu>
2019-01-22 18:12:11 +0100

    removed utils - repackageing of hidden state because we do not use hidden states past the minibatch

--- utils.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-
-def repackage_hidden(h):
-    """Wraps hidden states in new Tensors, to detach them from their history."""
-    if isinstance(h, torch.Tensor):
-        return h.detach()
-    else:
-        return tuple(repackage_hidden(v) for v in h)
