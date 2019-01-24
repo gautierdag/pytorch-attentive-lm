fixed exp overflow

train.py
@@ -10,7 +10,7 @@ def evaluate(model, data_iterator, criterion):
     total_loss = 0.
     example_count = 0
     with torch.no_grad():
-        for i, batch in tqdm(enumerate(data_iterator), total=len(data_iterator), disable=True):
+        for _, batch in tqdm(enumerate(data_iterator), total=len(data_iterator), disable=True):
             data, targets = batch.text.t(), batch.target.t().contiguous()
             output = model(data)
             output_flat = output.view(-1, model.vocab_size)
@@ -25,7 +25,7 @@ def evaluate(model, data_iterator, criterion):
 def train(args, model, train_iter, valid_iter,
           criterion, optimizer,
           iteration_step, epoch,
-          writer, scheduler):
+          writer,):
 
     # Turn on training mode which enables dropout.
     model.train()
@@ -55,27 +55,19 @@ def train(args, model, train_iter, valid_iter,
         if iteration_step % args.log_interval == 0 and i > 0:
             cur_loss = total_loss / total_num_examples
             elapsed = time.time() - start_time
+            exp_cur_loss = min(cur_loss, 7)
+            # note printing innacurat perplexity if high - to not clog ouput
             print('| epoch {:3d} | {}/{} batches | ms/batch {:5.2f} \
                 | loss {:5.2f} | ppl {:8.2f}'.format(epoch,
                                                      iteration_step, len(
                                                          train_iter)*args.epochs,
                                                      elapsed * 1000 / args.log_interval,
-                                                     cur_loss, math.exp(cur_loss)))
+                                                     cur_loss, min(math.exp(exp_cur_loss), 1000)))
             writer.add_scalar('training_loss', cur_loss, iteration_step)
             writer.add_scalar('training_perplexity',
-                              min(math.exp(cur_loss), 1000), iteration_step)
+                              min(math.exp(exp_cur_loss), 1000), iteration_step)
             total_loss = 0
             total_num_examples = 0
             start_time = time.time()
-
-        if iteration_step % args.optimization_step == 0 and iteration_step > 0:
-
-            loss = evaluate(model, valid_iter, criterion)
-            scheduler.step(loss)
-            writer.add_scalar('validation_loss', loss, iteration_step)
-
-            # track learning rate
-            writer.add_scalar(
-                'lr', optimizer.param_groups[0]['lr'], iteration_step)
 
     return iteration_step