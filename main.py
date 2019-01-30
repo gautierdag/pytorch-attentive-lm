f78070e8e225a677fb20379179d819b60ac76801
Gautier Dagan <gautierdagan2017@u.northwestern.edu>
2019-01-30 14:17:49 +0100

    remove bug test

--- main.py
+++ main.py
@@ -90,11 +90,6 @@ def main(args):
 
     args = parser.parse_args(args)
 
-    # Basic
-    args.optim = 'adam'
-    args.lr = 0.0001
-    # args.no_positional_attention = False
-
     if not args.file_name:
         run_name = generate_filename(args)
     else:
@@ -133,7 +128,7 @@ def main(args):
                               lr=args.lr, weight_decay=12e-7)
     if args.optim == 'adam':
         optimizer = optim.Adam(model.parameters(), lr=args.lr,
-                               betas=(0.0, 0.999), eps=1e-2,
+                               betas=(0.0, 0.999), eps=1e-8,
                                weight_decay=12e-7, amsgrad=True)
 
     criterion = nn.CrossEntropyLoss()
